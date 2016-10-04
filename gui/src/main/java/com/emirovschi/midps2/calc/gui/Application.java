217b2b72bd91609ab2cb4cb284204a14a248b5de
Eugen Mirovschi <emirovschi@vmists.com>
2016-10-04 17:36:44 +0300

    Adding empty main window

---
+++ gui/src/main/java/com/emirovschi/midps2/calc/gui/Application.java
@@ -0,0 +1,13 @@
+package com.emirovschi.midps2.calc.gui;
+
+import org.apache.pivot.wtk.DesktopApplicationContext;
+
+import java.io.IOException;
+
+public class Application
+{
+    public static void main(final String[] args) throws IOException
+    {
+        DesktopApplicationContext.main(MainWindow.class, args);
+    }
+}
