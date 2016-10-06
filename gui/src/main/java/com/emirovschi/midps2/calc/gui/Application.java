af37f1097b783895832c2448f449e964e023caaf
Eugen Mirovschi <emirovschi@vmists.com>
2016-10-06 15:10:10 +0300

    Moving window initialization logic to Application class

--- gui/src/main/java/com/emirovschi/midps2/calc/gui/Application.java
+++ gui/src/main/java/com/emirovschi/midps2/calc/gui/Application.java
@@ -1,16 +1,49 @@
 package com.emirovschi.midps2.calc.gui;
 
+import org.apache.pivot.beans.BXMLSerializer;
+import org.apache.pivot.collections.Map;
 import org.apache.pivot.wtk.DesktopApplicationContext;
+import org.apache.pivot.wtk.Display;
+import org.apache.pivot.wtk.Window;
 
 import java.io.IOException;
 
-public class Application
+public class Application implements org.apache.pivot.wtk.Application
 {
     public static final String WINDOW_PROPERTIES = "/window.properties";
+
+    private Window window;
 
     public static void main(final String[] args) throws IOException
     {
         final ArgumentBuilder argumentBuilder = new ArgumentBuilder(WINDOW_PROPERTIES);
-        DesktopApplicationContext.main(MainWindow.class, argumentBuilder.get());
+        DesktopApplicationContext.main(Application.class, argumentBuilder.get());
+    }
+
+    @Override
+    public void startup(final Display display, final Map<String, String> map) throws Exception
+    {
+        final BXMLSerializer bxmlSerializer = new BXMLSerializer();
+        window = (Window) bxmlSerializer.readObject(MainWindow.class, "/mainWindow.bxml");
+        window.open(display);
+    }
+
+    @Override
+    public boolean shutdown(final boolean b) throws Exception
+    {
+        window.close();
+        return false;
+    }
+
+    @Override
+    public void suspend() throws Exception
+    {
+
+    }
+
+    @Override
+    public void resume() throws Exception
+    {
+
     }
 }
--- gui/src/main/java/com/emirovschi/midps2/calc/gui/MainWindow.java
+++ gui/src/main/java/com/emirovschi/midps2/calc/gui/MainWindow.java
@@ -1,47 +1,16 @@
 package com.emirovschi.midps2.calc.gui;
 
-import org.apache.pivot.beans.BXMLSerializer;
 import org.apache.pivot.beans.Bindable;
 import org.apache.pivot.collections.Map;
 import org.apache.pivot.util.Resources;
-import org.apache.pivot.wtk.Application;
-import org.apache.pivot.wtk.Display;
 import org.apache.pivot.wtk.Window;
 
 import java.net.URL;
 
-public class MainWindow extends Window implements Application, Bindable
+public class MainWindow extends Window implements Bindable
 {
     @Override
     public void initialize(final Map<String, Object> map, final URL url, final Resources resources)
     {
-        
-    }
-
-    @Override
-    public void startup(final Display display, final Map<String, String> map) throws Exception
-    {
-        final BXMLSerializer bxmlSerializer = new BXMLSerializer();
-        final Window window = (Window) bxmlSerializer.readObject(MainWindow.class, "/mainWindow.bxml");
-        window.open(display);
-    }
-
-    @Override
-    public boolean shutdown(final boolean b) throws Exception
-    {
-        this.close();
-        return false;
-    }
-
-    @Override
-    public void suspend() throws Exception
-    {
-
-    }
-
-    @Override
-    public void resume() throws Exception
-    {
-
     }
 }
