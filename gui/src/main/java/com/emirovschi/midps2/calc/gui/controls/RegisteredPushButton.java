c9e5cb39e3c1bcc4d2956dbe471b75d955e65d95
Eugen Mirovschi <emirovschi@vmists.com>
2016-10-08 12:56:20 +0300

    Adding key binding for buttons

--- gui/src/main/java/com/emirovschi/midps2/calc/gui/controls/RegisteredPushButton.java
+++ gui/src/main/java/com/emirovschi/midps2/calc/gui/controls/RegisteredPushButton.java
@@ -3,11 +3,40 @@ package com.emirovschi.midps2.calc.gui.controls;
 import com.emirovschi.midps2.calc.gui.ButtonRegister;
 import org.apache.pivot.wtk.PushButton;
 
-@SuppressWarnings("unchecked")
+import java.util.Collections;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public abstract class RegisteredPushButton extends PushButton
 {
+    private Set<Character> keys = Collections.emptySet();
+
     public void setRegister(final ButtonRegister register)
     {
         register.register(this);
+    }
+
+    public Set<Character> getKeys()
+    {
+        return keys;
+    }
+
+    public void setKeys(final String keys)
+    {
+        this.keys = Stream.of(keys.split(","))
+                .map(this::convert)
+                .collect(Collectors.toSet());
+    }
+
+    private char convert(final String key)
+    {
+        if (key.charAt(0) == '\\')
+        {
+            final String ascii = key.substring(1, key.length());
+            return (char) Integer.parseInt(ascii);
+        }
+
+        return key.charAt(0);
     }
 }
